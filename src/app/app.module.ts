8e275da7544fac43c2451cf17bb369272ac53b9b
sheikh.wasit <sheikh.wasit@gmail.com>
2018-05-13 17:24:35 +0530

    hotel component  added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HotelComponent } from './hotel/hotel.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HotelComponent
   ],
   imports: [
     BrowserModule
