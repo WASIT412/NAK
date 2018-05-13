6a4e320aaff7f813f2d2aef6e35848f1a6f535a6
sheikh.wasit <sheikh.wasit@gmail.com>
2018-05-13 17:39:07 +0530

    tour component add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HotelComponent } from './hotel/hotel.component';
+import { TourComponent } from './tour/tour.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HotelComponent
+    HotelComponent,
+    TourComponent
   ],
   imports: [
     BrowserModule
