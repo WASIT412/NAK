ab1b290f33f659838956851a34527eff8fe0e14c
Wasit Sheikh <sheikh.wasit@gmail.com>
2018-05-16 22:35:19 +0530

    Controls added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  frm: FormGroup;
+  checkboxOptions: any = [
+    { label: 'Admin Access Only', controlName: 'adminOnly' },
+    { label: 'Require Password', controlName: 'reqPassword' },
+    { label: 'Apply Login Timeout', controlName: 'loginTimeout' }
+  ];
+  data: any = [
+    {State : 'J&K', ID: '1'},
+    {State : 'Delhi', ID: '2'},
+    {State : 'Haryana', ID: '3'},
+    {State : 'Punjab', ID: '4'}
+  ];
+
+  constructor(private formBuilder: FormBuilder) {
+     this.frm = this.formBuilder.group({
+       email: new FormControl('', [Validators.required]),
+       cb: new FormControl(),
+       adminOnly: new FormControl(),
+      reqPassword: new FormControl(),
+      loginTimeout: new FormControl(),
+      dropdown: new FormControl(),
+      radiobutton: new FormControl(),
+      datePick: new FormControl()
+      });
+   }
+
+   onSelection($event) {
+console.log($event);
+   }
+   radioChanged($event) {
+    console.log($event);
+   }
 }
+//}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { HotelComponent } from './hotel/hotel.component';
+import { CyberintechControlsModule } from './framework/controls/controls.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HotelComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CyberintechControlsModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
